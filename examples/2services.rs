16579bba99550ef31472345bb3db32385acd78ab
Peter Heiss <peter.heiss@uni-muenster.de>
2023-09-13 19:16:32 +0200

    fix example

--- examples/2services.rs
+++ examples/2services.rs
@@ -1,4 +1,5 @@
 use std::thread;
+use std::thread::sleep;
 use types::String;
 
 fn main() {
@@ -6,22 +7,27 @@ fn main() {
         let client = redis::Client::open("redis://localhost:6379").unwrap();
         let client2 = client.clone();
 
-        s.spawn(move || {
-            let string = String::with_value("Hello".to_string(), "test", client);
-            // prints out "Hello"
-            println!("string: {}", string.cached().unwrap());
+        let t1 = s.spawn(move || {
+            let mut string = String::with_value("Hello".to_string(), "test", client);
+            println!("Thread1: {}", string.cached().unwrap());
             assert_eq!(string, "Hello");
-        })
-        .join()
-        .expect("Failed to join thread1");
+            sleep(std::time::Duration::from_secs(1));
+            string.store("World".to_string());
+            println!("Thread1: {}", string.cached().unwrap());
+            assert_eq!(string, "World");
+        });
 
-        s.spawn(move || {
-            let string = String::with_value_load("test", client2);
-            // prints out "Hello"
-            println!("string: {}", string.cached().unwrap());
+        let t2 = s.spawn(move || {
+            sleep(std::time::Duration::from_micros(100));
+            let mut string = String::with_load("test", client2);
+            println!("Thread2: {}", string.cached().unwrap());
             assert_eq!(string, "Hello");
-        })
-        .join()
-        .expect("Failed to join thread2");
+            sleep(std::time::Duration::from_secs(2));
+            string.acquire();
+            println!("Thread2: {}", string.cached().unwrap());
+            assert_eq!(string, "World");
+        });
+        t1.join().expect("Failed to join thread1");
+        t2.join().expect("Failed to join thread2");
     });
 }
